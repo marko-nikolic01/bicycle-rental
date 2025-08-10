795010f0489011399bd3a479e66fc1d26a034e6d
Marko <nikolicmarko1243@gmail.com>
2025-08-10 18:24:46 +0200

    feature: Initialized local bicycle rental app.

---
+++ local-bicycle-rental/app/controller/registration_controller.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.repository import UserRepository
+from app.service import RegistrationService
+from app.dto import RegistrationDTO, RegistrationSuccessDTO
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+@router.post("/", response_model=RegistrationSuccessDTO, status_code=status.HTTP_201_CREATED)
+def register(registration_dto: RegistrationDTO, db: Session = Depends(get_db)):
+    user_repository = UserRepository(db)
+    registration_service = RegistrationService(user_repository)
+    return registration_service.register(registration_dto)
