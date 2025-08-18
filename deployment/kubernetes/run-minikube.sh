687c42d81563a94e1b15911a034bc79770a81ec1
Marko <nikolicmarko1243@gmail.com>
2025-08-18 17:54:59 +0200

    feature: Ingress and egress limitation.

--- deployment/kubernetes/run-minikube.sh
+++ deployment/kubernetes/run-minikube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minikube start --driver=docker --cni calico
+minikube start --driver=docker --cni=calico
 minikube addons enable storage-provisioner
 minikube addons enable ingress
 eval $(minikube -p minikube docker-env)
